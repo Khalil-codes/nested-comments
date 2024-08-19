222b4270e5bc37f0abf05df01f1961c6cb5a0f83
Khalil <khalilpati55@gmail.com>
2024-08-19 23:01:09 +0530

    Added Tailwind and Shadcn to build UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import PostCard from "@/components/post-card";
+import { fetchPosts } from "@/services/posts";
+import Link from "next/link";
+
+export default async function Home() {
+  const posts = await fetchPosts();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Helo World</h1>
+    <main className="flex flex-col gap-4">
+      <h1 className="my-10 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Posts
+      </h1>
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post.id} href={`/posts/${post.id}`} className="group">
+            <PostCard post={post} />
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
